ee7056a2d5c7e30ac47fcce9356ad44cd797d87c
lakeedrock <lakeedrock@gmail.com>
2020-07-29 00:32:39 +0300

    03 First Project v-for directive added

--- 03_first_project/script.js
+++ 03_first_project/script.js
@@ -1,53 +1,55 @@
-const model_1 = {
-    url: 'images/1.jpg',
-    title: 'First image title',
-    headding: 'Headding One',
-    content: 'This is the decription of headding one. This is a text content. Summary of he content.',
-    category: 'First Category'
-}
-
-const model_2 = {
-    url: 'images/2.jpg',
-    title: 'Two image title',
-    headding: 'Headding Two',
-    content: 'This is the decription of headding two. This is a text content. Summary of he content.',
-    category: 'Two Category'
-}
-
-const model_3 = {
-    url: 'images/3.jpg',
-    title: 'Three image title',
-    headding: 'Headding Three',
-    content: 'This is the decription of headding three. This is a text content. Summary of he content.',
-    category: 'Three Category'
-}
-
-const model_4 = {
-    url: 'images/4.jpg',
-    title: 'Four image title',
-    headding: 'Headding Three',
-    content: 'This is the decription of headding four. This is a text content. Summary of he content.',
-    category: 'Four Category'
-}
-
-const model_5 = {
-    url: 'images/5.jpg',
-    title: 'Five image title',
-    headding: 'Headding Three',
-    content: 'This is the decription of headding five. This is a text content. Summary of he content.',
-    category: 'Five Category'
-}
-
-const model_6 = {
-    url: 'images/6.jpg',
-    title: 'Six image title',
-    headding: 'Headding Three',
-    content: 'This is the decription of headding six. This is a text content. Summary of he content.',
-    category: 'Six Category'
-}
-
-
 const vm = new Vue({
-    el: "#app",
-    data: model_6
+  el: "#app",
+  data: {
+    data_set: [
+      {
+        url: "images/1.jpg",
+        title: "First image title",
+        headding: "Headding One",
+        content:
+          "This is the decription of headding one. This is a text content. Summary of he content.",
+        category: "First Category",
+      },
+      {
+        url: "images/2.jpg",
+        title: "Two image title",
+        headding: "Headding Two",
+        content:
+          "This is the decription of headding two. This is a text content. Summary of he content.",
+        category: "Two Category",
+      },
+      {
+        url: "images/3.jpg",
+        title: "Three image title",
+        headding: "Headding Three",
+        content:
+          "This is the decription of headding three. This is a text content. Summary of he content.",
+        category: "Three Category",
+      },
+      {
+        url: "images/4.jpg",
+        title: "Four image title",
+        headding: "Headding Three",
+        content:
+          "This is the decription of headding four. This is a text content. Summary of he content.",
+        category: "Four Category",
+      },
+      {
+        url: "images/5.jpg",
+        title: "Five image title",
+        headding: "Headding Three",
+        content:
+          "This is the decription of headding five. This is a text content. Summary of he content.",
+        category: "Five Category",
+      },
+      {
+        url: "images/6.jpg",
+        title: "Six image title",
+        headding: "Headding Three",
+        content:
+          "This is the decription of headding six. This is a text content. Summary of he content.",
+        category: "Six Category",
+      },
+    ],
+  },
 });
