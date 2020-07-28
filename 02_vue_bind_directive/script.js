254fba3e11f813557694ad2cd47101b77badde9d
Lakshan Costa <lakshan.costa@7p-group.com>
2020-07-28 16:25:29 +0300

    Vue bind directive

---
+++ 02_vue_bind_directive/script.js
@@ -0,0 +1,11 @@
+
+const model = {
+    name: '<b>Lakshan</b>',
+    url: '../assets/images/vue_logo.png',
+    title: 'I am the logo of VUE'
+}
+
+const vm = new Vue({
+    el: '#app',
+    data: model,
+});
