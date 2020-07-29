beae9c8c7af4612be2b695fb55bcc4732225db7c
lakeedrock <lakeedrock@gmail.com>
2020-07-29 03:30:06 +0300

    04 Vue for directive

---
+++ 04_vue_for_directive/script.js
@@ -0,0 +1,42 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    data_set: [
+      {
+        brand: "Toyota",
+        model: "Corolla",
+        engine: "1.6L",
+        transmission: "Automatic",
+        price: "12000 USD",
+      },
+      {
+        brand: "Nissan",
+        model: "GTR",
+        engine: "5.5L",
+        transmission: "Triptronic",
+        price: "52000 USD",
+      },
+      {
+        brand: "BMW",
+        model: "M3 Sport",
+        engine: "2.6L",
+        transmission: "Automatic",
+        price: "18000 USD",
+      },
+      {
+        brand: "Mercedez Benz",
+        model: "C Class",
+        engine: "2.6L",
+        transmission: "Automatic",
+        price: "16000 USD",
+      },
+      {
+        brand: "Mazda",
+        model: "Mazda 6",
+        engine: "1.6L",
+        transmission: "Manual",
+        price: "7500 USD",
+      },
+    ],
+  },
+});
